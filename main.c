56319ce5d8e16dcc0ff13c9afce6b234ad2f45f0
ConstAndTime <nbfbirov@mail.ru>
2021-07-18 20:38:46 +0300

    Add homework Files

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (const int argc, const char** argv) {
+int arr[100] = {2, 3};
+int capacity = 2;
+for (int i = 4; i < 100; ++i) {
+    int p = 1;
+    for (int j = 0; j < capacity; ++j) {
+        if (i % arr[j] == 0) {
+            p = 0;
+            break;
+        }
+    }
+    if (p) {
+        arr[capacity++] = i;
+    }
+}
+for (int k = 0; k < capacity; ++k) {
+    printf("%d ", arr[k]);
+}
+return 0;
+}
